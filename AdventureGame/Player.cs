d58c3692831697daa89ecedb3586383decc346a7
Karl Fristedt <karl.fristedt@yh.nackademin.se>
2017-09-19 15:40:19 +0200

    Player class Room class

---
+++ AdventureGame/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Player
+    {
+        // Properties
+        // - Playername
+        // - Inventory (list of items)
+        // - IsAlive (bool)
+
+
+
+        // Metoder
+        // - Go, pickup, drop, look, use, open, die
+
+    }
+}
---
+++ AdventureGame/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Room
+    {
+        // Properties
+        // - Description (text about room)
+        // - Inventory of items
+        // - Exits
+
+        // Methods
+        // 
+        // PrintDescription (Print description of room)
+
+
+
+
+    }
+}
