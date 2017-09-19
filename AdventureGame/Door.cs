26ea87f6cf56251e02e9cd8665f56fb2c0a52aba
Elias Mohammed <eliasrmohammed@hotmail.com>
2017-09-19 15:38:41 +0200

    Items och dorr class

---
+++ AdventureGame/Door.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventureGame
+{
+    class Door
+    {
+        //Här finns dörrar mellan rummen.
+        //Vissa dörrar är öppna. 
+        //Vissa dörrar behöver nycklar från klass item för att öppns.
+
+
+    }
+}
